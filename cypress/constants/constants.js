a27be3d3aa6b916c2ec11aad816291dee7e3dad8
Petra M <pm.lovac@gmail.com>
2023-09-12 12:00:46 +0200

    sort inventory refactor using constants

---
+++ cypress/constants/constants.js
@@ -0,0 +1,18 @@
+
+export const pageURLs = {
+  logingPage: 'https://www.saucedemo.com/v1/.html',
+  inventoryPage: 'https://www.saucedemo.com/v1/inventory.html'
+};
+export const productNames = {
+  hilo: 'Sauce Labs Fleece Jacket',
+  lohi: 'Sauce Labs Onesie',
+  az: 'Sauce Labs Backpack',
+  za: 'Test.allTheThings() T-Shirt (Red)'
+};
+export const sortingOptions = [
+  { value: 'hilo', text: 'Price (high to low)' },
+  { value: 'lohi', text: 'Price (low to high)' },
+  { value: 'az', text: 'Name (A to Z)' },
+  { value: 'za', text: 'Name (Z to A)' }
+];
+
--- cypress/e2e/CartTest/SortInventory.spec.js
+++ cypress/e2e/CartTest/SortInventory.spec.js
@@ -1,40 +1,30 @@
 ///<reference types = "cypress"/>
 
+import { sortingOptions, productNames, pageURLs } from '../../constants/constants.js';
+
 describe('Sort Inventory Test', () => {
-    const sortingOptions = [
-        { option: 'hilo', text: 'Price (high to low)' },
-        { option: 'lohi', text: 'Price (low to high)' },
-        { option: 'az', text: 'Name (A to Z)' },
-        { option: 'za', text: 'Name (Z to A)' }
-    ];
+    it('should log in with a standard user', () => {
+        // Log in with a standard user 
+        cy.login();
+        // Verify that the login was successful
+        cy.url().should('eq', pageURLs.inventoryPage);
 
-    describe('Sort Inventory Test', () => {
-        const sortingOptions = [
-            { option: 'hilo', text: 'Price (high to low)', itemName: 'Sauce Labs Fleece Jacket' },
-            { option: 'lohi', text: 'Price (low to high)', itemName: 'Sauce Labs Onesie' },
-            { option: 'az', text: 'Name (A to Z)', itemName: 'Sauce Labs Backpack' },
-            { option: 'za', text: 'Name (Z to A)', itemName: 'Test.allTheThings() T-Shirt (Red)' }
-        ];
+        for (const { value, text } of sortingOptions) {
+            // Click on sort dropdown
+            cy.get('select[class="product_sort_container"]').select(value);
 
-        it('should log in with a standard user', () => {
-            // Step 1: Log in with a standard user 
-            cy.login();
-            // Verify that the login was successful
-            cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
+            // Verify that the dropdown menu is showing the selected option
+            cy.get('select[class="product_sort_container"]').should('have.value', value);
+            cy.get('select[class="product_sort_container"] option:selected').should('have.text', text);
 
-            for (const { option, text, itemName } of sortingOptions) {
-                // Click on sort dropdown
-                cy.get('select[class="product_sort_container"]').select(option);
-
-                // Verify that the dropdown menu is showing the selected option
-                cy.get('select[class="product_sort_container"]').should('have.value', option);
-                cy.get('select[class="product_sort_container"] option:selected').should('have.text', text);
-
-                // Get the name of the first item after sorting
-                cy.get('.inventory_item_name')
-                    .first()
-                    .should('have.text', itemName);
-            }
-        });
+            // Get the name of the first item after sorting
+            cy.get('.inventory_item_name')
+                .first()
+                .should('have.text', productNames[value]);
+        }
+        console.log(productNames.az);
+        console.log(sortingOptions[0].text);
     });
-})
+});
+
+
